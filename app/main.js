e3e93b81bac8b4c84849223ed69facd19ed47582
LuanaChavesFreire <luanachavesfreire2006@gmail.com>
2026-02-13 20:19:07 -0300

    mudando elementos DOM

---
+++ app/main.js
@@ -0,0 +1,43 @@
+const html = document.querySelector("html");
+const focoBtn = document.querySelector(".appCardButton--foco");
+const descansoCurtoBtn = document.querySelector(".appCardButton--curto");
+const descansoLongoBtn = document.querySelector(".appCardButton--longo");
+const imgBanner = document.querySelector(".appImage");
+const titulo = document.querySelector(".appTitle");
+const btns = document.querySelectorAll('.appCardButton')
+
+function alterarContexto(contexto) {
+  html.setAttribute("dataContexto", contexto);
+  imgBanner.src = `/imagens/${contexto}.png`;
+  switch (contexto) {
+    case "foco":
+      titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="appTitleStrong">mergulhe no que importa.</strong>`;
+      break;
+    case "descansoCurto":
+      titulo.innerHTML = `Que tal dar uma respirada?<br>
+                <strong class="appTitleStrong">Faça uma pausa curta.</strong>`;
+      break;
+    case "descansoLongo":
+      titulo.innerHTML = `Hora de voltar à superficie.<br>
+                <strong class="appTitleStrong">Faça uma pausa longa.</strong>`;
+  }
+  btns.forEach((contexto) => {
+    contexto.classList.remove('active')
+  })
+}
+
+focoBtn.addEventListener("click", () => {
+  alterarContexto("foco");
+  focoBtn.classList.add('active')
+});
+
+descansoCurtoBtn.addEventListener("click", () => {
+  alterarContexto("descansoCurto");
+  descansoCurtoBtn.classList.add('active')
+});
+
+descansoLongoBtn.addEventListener("click", () => {
+  alterarContexto("descansoLongo");
+  descansoLongoBtn.classList.add('active')
+});
